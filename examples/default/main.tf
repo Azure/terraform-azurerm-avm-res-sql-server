1097253efe52329854e19d280d120521a130d3f3
mbilalamjad <79409563+mbilalamjad@users.noreply.github.com>
2024-09-05 22:36:23 -0700

    Update

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,13 +1,17 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = "~> 1.6"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.7.0, < 4.0.0"
+      version = "~> 3.108"
+    }
+    modtm = {
+      source  = "Azure/modtm"
+      version = "~> 0.3"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.5.1"
+      version = "~> 3.6"
     }
   }
 }
