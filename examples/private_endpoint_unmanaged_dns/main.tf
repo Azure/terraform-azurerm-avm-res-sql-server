64fa3289c7f58dbefea8cc32c13f146f7257210f
Hein Tonny Køien <hein.tonny.koien@hafslund.no>
2025-01-06 16:24:55 +0100

    Add example for private_endpoint_unmanaged_dns

---
+++ examples/private_endpoint_unmanaged_dns/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_version = "~> 1.6"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.108"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.3.0"
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  location = "AustraliaEast"
+  name     = module.naming.resource_group.name_unique
+}
+
+resource "azurerm_virtual_network" "this" {
+  address_space       = ["192.168.0.0/24"]
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.virtual_network.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_subnet" "this" {
+  address_prefixes     = ["192.168.0.0/24"]
+  name                 = module.naming.subnet.name_unique
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+}
+
+resource "azurerm_private_dns_zone" "this" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "random_password" "admin_password" {
+  length           = 16
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+  special          = true
+}
+
+# This is the module call
+module "sql_server" {
+  source = "../../"
+  # source             = "Azure/avm-<res/ptn>-<name>/azurerm"
+  # ...
+  enable_telemetry              = false
+  name                          = module.naming.sql_server.name_unique
+  resource_group_name           = azurerm_resource_group.this.name
+  administrator_login           = "mysqladmin"
+  administrator_login_password  = random_password.admin_password.result
+  public_network_access_enabled = false
+  location                      = azurerm_resource_group.this.location
+  server_version                = "12.0"
+  private_endpoints = {
+    primary = {
+      private_dns_zone_resource_ids = [azurerm_private_dns_zone.this.id]
+      subnet_resource_id            = azurerm_subnet.this.id
+    }
+  }
+  # This is required to disable the DNS zone group management.
+  # If you want to manage the DNS zone group externally, ex. with Azure Policy as in Enterprise Scale corp, you must set this to false.
+  private_endpoints_manage_dns_zone_group = false
+}
